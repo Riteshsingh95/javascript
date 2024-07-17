2a0e2a67f2e60dbb48274812f94073af1ecdd316
Ritesh Singh <145100862+Riteshsinghgkp@users.noreply.github.com>
2024-07-17 21:44:02 +0000

    container config done

---
+++ .devcontainer/1_basics/1_variables.js
@@ -0,0 +1,11 @@
+const accountID=951711
+let accountEmail = "singhritesh@gmail.com"
+var accountPassword = "123456"
+let accountCity = "gorakhpur" 
+let accountState; // here we are not put any value in accountState , then it is undefined 
+
+// accountID = 2  // not allowed for change the const value i
+
+// console.log(accountID) //
+
+console.table([accountEmail,accountID,accountPassword,accountCity,accountState])
