5773ec1d0bf896455a60e358ba6adc89f5e572ec
Ritesh Singh <145100862+Riteshsinghgkp@users.noreply.github.com>
2024-07-18 21:08:21 +0000

    container config done

---
+++ .devcontainer/1_basics/3_conversionoperation.js
@@ -0,0 +1,40 @@
+/*let score = 40
+
+let stringscore= String(score)
+console.log(stringscore);
+
+console.log(typeof stringscore); */
+
+/* 
+let score = "50"
+
+let numberscore= Number(score) // string data types is not changable from another datatypes
+console.log(numberscore);
+
+console.log( typeof score);
+*/
+
+
+
+/*
+let score= null
+
+//let BooleanINscore= Boolean(score) // string data types is not changable from another datatypes
+//console.log(BooleanINscore);
+
+console.log( typeof score);
+
+let valueINnumber= Number(score)
+console.log(typeof valueINnumber);
+console.log(valueINnumber);
+*/
+
+let score= "Ritesh" // it can not change in number
+    
+// let score = "222" // it change in number 
+
+console.log( typeof score);
+
+let valueINnumber= Number(score)
+console.log(typeof valueINnumber);
+console.log(valueINnumber);
