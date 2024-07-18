79177db6dbaca626a0f91fe3a83201ce18aca10d
Ritesh Singh <145100862+Riteshsinghgkp@users.noreply.github.com>
2024-07-18 22:19:18 +0000

    container config done

---
+++ .devcontainer/1_basics/4_comparision.js
@@ -0,0 +1,7 @@
+ console.log(2>3);
+
+ console.log("3">"2");
+
+ console.log("4">=4);
+
+ console.log(4==="5");// here when we are using === operator then its not allow to compare diffrent diffrent datatypes togather
