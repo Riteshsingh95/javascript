1a9214eadcbe613b8b741751230d1f29dcee0244
Ritesh Singh <145100862+Riteshsinghgkp@users.noreply.github.com>
2024-07-19 23:27:05 +0000

    container config done

--- .devcontainer/1_basics/6_stack_heap.js
+++ .devcontainer/1_basics/6_stack_heap.js
@@ -12,6 +12,9 @@ anothername = "Little"
 console.log(anothername);
 console.log(myname);
 
+/* stack: inka value directly stack memory mein store hota hai . jab aap inhe ek variable se 
+dusre variable mein asign krte hain , to ye ek copy bnati hain */
+
 
 //Heap
 
@@ -27,3 +30,6 @@ userTwo.email = "little@gmail.com"
 
 console.log(userOne.email);
 console.log(userTwo.email);
+
+/* heap: inka refrence stack mein store hota hai aur actual data heap mein hota hai,jab aap inhe 
+ asign krte hain , refrence copy hota hai , orignal data nahi . */
