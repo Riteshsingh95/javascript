c90f2da6848e76cdd32b9d1001b7a1c6ae74c817
Ritesh Singh <145100862+Riteshsinghgkp@users.noreply.github.com>
2024-07-23 00:00:10 +0000

    container config done

---
+++ .devcontainer/2_basics/1_array.js
@@ -0,0 +1,17 @@
+
+const myArr = [5,7,3,2,1]
+
+// console.log(myArr);
+
+// Array methods
+
+myArr.push(8)
+myArr.push(10)
+myArr.push(4)
+myArr.push(12)
+myArr.pop()
+myArr.pop()
+// console.log("my arr is:");
+console.log(myArr);
+
+
