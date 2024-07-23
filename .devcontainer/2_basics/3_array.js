226364e1524085a4d2a06517c80ec60003069cf9
Ritesh Singh <145100862+Riteshsinghgkp@users.noreply.github.com>
2024-07-23 22:51:26 +0000

    container config done

--- .devcontainer/2_basics/3_array.js
+++ .devcontainer/2_basics/3_array.js
@@ -1,17 +1,20 @@
 
+/*
+
 const ind_team = ["MS Dhoni","Rohit","virat"]
 
 const Aus_team = ["Maxwell","Starc","Warner"]
 
 const newz_team = ["boult","conway","sentner"]
 
-const IPL = ind_team.concat(...Aus_team, ...newz_team)  // concat func()  se hm 2 arrays ko merge kr sakte hain
+const IPL = ind_team.concat(...Aus_team, ...newz_team)  // concat func()  se hm 2 ya usse jyada arrays ko merge kr sakte hain
 
 console.log("Ind",ind_team);
 console.log("Aus",Aus_team);
 console.log("Newz",newz_team);
 console.log("iPL",IPL);
 
+*/
 
 const annother_array = [1,2,3,[5,4,3],10,30,[11,22,33,44,[50,40,70,60]]]
 const real_another_array = annother_array.flat(Infinity)
@@ -20,3 +23,10 @@ const real_another_array = annother_array.flat(Infinity)
 
 console.log("another_array",annother_array);
 console.log("real_another_array",real_another_array);
+
+
+console.log(Array.isArray("Ritesh"));
+// string to array conversion 
+console.log(Array.from("Ritesh")); // using from
+
+console.log(Array.from({name:"Ritesh"}));  // yha pe ye hame empty array dega 
