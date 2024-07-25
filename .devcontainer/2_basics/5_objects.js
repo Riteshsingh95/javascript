f5c5973428b9cbf9dcac1387eb4d987ed0175191
Ritesh Singh <145100862+Riteshsingh95@users.noreply.github.com>
2024-07-25 20:46:06 +0000

    container config done

--- .devcontainer/2_basics/5_objects.js
+++ .devcontainer/2_basics/5_objects.js
@@ -2,6 +2,8 @@
 
 // singleton
 
+
+/*
 const obj1 = {1:"one",2:"two"}
 const obj2 = {3:"three",4:"four"}
 const obj3 = {5:"five",6:"six"}
@@ -9,3 +11,24 @@ const obj3 = {5:"five",6:"six"}
 const obj4 = {...obj1,...obj2,...obj3}  // spred operator
 
 console.log("obj4 is",obj4);
+
+*/
+
+/******************************** Object Nesting ************************/
+
+const app = {
+    email:"ritesh@gmail.com",  // string
+    username:{         // object
+        fullname:{
+            firstname:"ritesh",
+            lastname:"singh"
+        }
+       
+    }
+}
+
+console.log(app.username.fullname);
+console.log(app.email);
+
+// console.log( typeof app.username.fullname);
+// console.log(typeof app.email);
