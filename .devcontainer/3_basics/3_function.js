a835e5aeefd4fc425c47f2d76427313974190469
Ritesh Singh <145100862+Riteshsingh95@users.noreply.github.com>
2024-07-26 13:02:30 +0000

    container config done

---
+++ .devcontainer/3_basics/3_function.js
@@ -0,0 +1,8 @@
+
+
+function calculatecartprice(...price) {        // yha pr rest operator use hua hai (...price) user kitni v value dega ye sb ek arraye mein aa jayengi
+    return price
+   
+}
+
+console.log(calculatecartprice(22,33,4,5,6,7,8,9,11,44,244));
