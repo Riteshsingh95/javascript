514b487f3d5e0c27dd1ab664c4156a1f75d05fb5
Ritesh Singh <145100862+Riteshsingh95@users.noreply.github.com>
2024-08-07 19:40:27 +0000

    container config done

--- .devcontainer/3_basics/8_arrow_function.js
+++ .devcontainer/3_basics/8_arrow_function.js
@@ -8,7 +8,9 @@ const user={
 
     welcomemassage: function() {
         console.log(`${this.username},welcome to our website`); // 'this' yahan 'user'object ko refer kar raha hai
-        
+
+         console.log(this);  // this keyword basically corrent context ko refer krta hai
+                 
     }
 }
 
@@ -22,4 +24,5 @@ user.welcomemassage()    // for "ram"
 user.username="raghu"    /// yha pr fir se username ki value ko change kiya gya hai
 user.welcomemassage()    // for "raghu"
 
-
+// console.log();  // yha pr jo global object h,ye empty{} object hoga
+// aur browser k ander jo global object hota hai wo window object hota hai
